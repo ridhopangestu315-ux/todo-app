7f018abb4e93f3c416fd26a8d7927140fbb1ea5a
Ridho Pangestu <ridhopangestu315@gmail.com>
2026-05-03 22:37:59 +0700

    perubahan4

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ function showPage(pageId, el) {
     el.classList.add('active');
   }
 }
+
+function tambahTugas() {
+  const select = document.getElementById("course");
+  const value = select.value;
+  const judul = document.getElementById("JudulTugas").value;
+
+  if (value === "") {
+    alert("Pilih mata kuliah dulu!");
+    return;
+  }
+
+  if (judul === "") {
+    alert("Masukkan nama tugas!");
+    return;
+  }
+
+  const list = document.querySelector(".tugas-list");
+
+const item = document.createElement("p");
+
+item.innerHTML = `
+  <span class="task-text">⏳ ${judul} - ${value}</span>
+  <button onclick="selesaiTugas(this)">✔️</button>
+  <button onclick="hapusTugas(this)">❌</button>
+`;
+
+  list.appendChild(item);
+
+  select.value = "";
+  document.getElementById("JudulTugas").value = "";
+}
+
+function selesaiTugas(btn) {
+  const text = btn.parentElement.querySelector(".task-text");
+
+  text.style.textDecoration = "line-through";
+  text.style.color = "gray";
+  text.textContent = text.textContent.replace("⏳", "✔️");
+}
+
+function hapusTugas(btn) {
+  btn.parentElement.remove();
+}
