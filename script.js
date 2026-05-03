6a8107feb35b13301550808bb7393540da82050c
Ridho Pangestu <ridhopangestu315@gmail.com>
2026-05-03 19:57:10 +0700

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPage(pageId, el) {
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => page.style.display = 'none');
+
+  document.getElementById(pageId).style.display = 'block';
+
+  const menuItems = document.querySelectorAll('.sidebar li');
+  menuItems.forEach(item => item.classList.remove('active'));
+
+  el.classList.add('active');
